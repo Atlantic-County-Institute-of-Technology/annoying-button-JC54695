3a817d18f77713313ca624d231899d72c98df2e3
JC54695 <clis0022@student.acitech.org>
2025-03-05 09:09:53 -0500

    Jimmy...

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const button1 = document.getElementById("button1");
+let text = document.getElementById("text")
 
-button1.addEventListener("click", yell_at_user);
+let clicks = 0;
 
-function yell_at_user(){
-    alert("STOP PRESSING THIS BUTTON!");
+button1.addEventListener("click", annoy )
+
+
+
+
+function annoy() {
+    clicks++;
+    console.log(clicks)
+    if( clicks == 1 ) {
+        text.innerHTML = "Hey! Stop That!";
+    }
+    else if( clicks == 5) {
+        text.innerHTML = "I said STOP!";
+    }
+    else if( clicks == 10) {
+        text.innerHTML = "This isn't funny. Please stop."
+    }
+    else if( clicks == 11) {
+        text.innerHTML = "I have your family..."
+    }
+    else if( clicks == 12) {
+        text.innerHTML = "Alright now stop!"
+    }
+    else if(clicks == 15) {
+        text.innerHTML = "Here click my friend instead!"
+    }
 }
+
